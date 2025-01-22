fd3326c7b1fb4ff7d109f746aafa8b604bb2cba0
Kanchana K. <162160183+Kanchana-Kaushal@users.noreply.github.com>
2025-01-22 21:13:09 +0530

    made the cart quantity icon interactive.

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -83,5 +83,13 @@ addToCartBtn.forEach((button) => {
                 quantity: 1,
             });
         }
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector(".js-cart-quantity-icon").innerHTML =
+            cartQuantity;
     });
 });
