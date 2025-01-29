0be1b0b9852277b10fd19288df940baf04f1a1ae
Kanchana K. <162160183+Kanchana-Kaushal@users.noreply.github.com>
2025-01-29 18:27:56 +0530

    Generate HTML for the order summery

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,14 @@
-export const cart = [];
+export const cart = [
+    {
+        productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+    },
+
+    {
+        productID: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+        quantity: 3,
+    },
+];
 
 export function addToCart(productID) {
     let existingItem;
